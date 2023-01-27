c51073ae58de2f48c9ba6499a6f824a580e4bc3b
Sundar paul Dakkumalla <sundarpaul002@gmail.com>
2023-01-27 18:41:28 +0530

    changes

--- src/components/Banners/ServiceTimingsBanner.js
+++ src/components/Banners/ServiceTimingsBanner.js
@@ -7,21 +7,21 @@ const ServiceTimingsBanner = () => {
   return (
     <div className='banner__pad__marg overflow w-full h-[500px] bg-[#F5F5DC] flex justify-center items-center'>
 
-        <div className='ring-container-right reponsive__banneer_serviceTimings'>
+        {/* <div className='ring-container-right reponsive__banneer_serviceTimings'>
             <div className='gold-ring flex justify-center items-center'>
                 <div className='inner-white'></div>
             </div>
-        </div>
+        </div> */}
         <div className='main-content'>
             <Typography className='header'>Service Timings</Typography>
-            <Typography className='content'>Sunday 9:00-11:00am</Typography>
+            <Typography className='content !lg:text-5xl !md:text-4xl'>Sunday 9:00-11:00am</Typography>
             <Typography className='content-link'>Online Serive: www.gcci/live.com</Typography>
         </div>
-        <div className='ring-container-left reponsive__banneer_serviceTimings'>
+        {/* <div className='ring-container-left reponsive__banneer_serviceTimings'>
             <div className='gold-ring flex justify-center items-center'>
                 <div className='inner-white'></div>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
--- src/components/VisionMissionBanner/VisionMissionBanner.js
+++ src/components/VisionMissionBanner/VisionMissionBanner.js
@@ -1,4 +1,3 @@
-import { Typography } from '@mui/material'
 import React from 'react'
 
 const VisionMissionBanner = () => {
@@ -6,14 +5,14 @@ const VisionMissionBanner = () => {
     <div className='banner__pad__marg text-center'>
       <div className='mx-10'>
         <div>
-          <Typography variant='h3' className='text-[#C10000] !mb-10 !md:text-[10px]'>VISION</Typography>
-          <div className='text-fill leading-relaxed flex items-center lg:text-2xl md:text-xl'>
+          <h1 className='text-[#C10000] mb-4 text-4xl font-medium leading-none tracking-tight md:text-4xl lg:text-5xl'>VISION</h1>
+          <div className='mb-6 text-lg font-normal text-black lg:text-xl sm:px-16 xl:px-48'>
             OUR VISION IS TO CREATE ENVIRONMENTS WHERE PEOPLE ARE ENCOURAGED AND INSPIRED TO DRAW CLOSER TO GOD, TO CONNECT WITH OTHERS IN MEANINGFUL AND LIFE GIVING RELATIONSHIPS, AND TO DISCOVER THEIR GIFTS AND USE THEM FOR THE BETTERMENT OF OTHERS.
           </div>
         </div>
         <div className='banner__pad__marg'>
-          <Typography variant='h3' className='text-[#C10000] !mb-10'>OUR MISSION IS TO LEAD PEOPLE INTO A LIFE-TRANSFORMING RELATIONSHIP WITH JESUS CHRIST </Typography>
-          <div className='text-fill text-2xl leading-relaxed flex items-center lg:text-2xl md:text-xl'>
+          <h2 className='text-[#C10000] mb-4 text-4xl font-medium leading-none tracking-tight md:text-4xl lg:text-5xl'>OUR MISSION IS TO LEAD PEOPLE INTO A LIFE-TRANSFORMING RELATIONSHIP WITH JESUS CHRIST </h2>
+          <div className='mb-6 text-lg font-normal text-black lg:text-xl sm:px-16 xl:px-48'>
           Whether it be through our Sunday services, small groups, mission trips, youth services, counseling, or growth classes, GCCI believes in giving you the tools you need to have a deeper relationship with God through Lord Jesus Christ.          </div>
         </div>
       </div>
